29ed7b4f4c39acfd19d4afb872cd9364d4c86d9d
Suriya <suriyapradeep2003@gmail.com>
2024-02-15 14:43:28 +0530

    System Sort

---
+++ src/Sort/SystemSort.java
@@ -0,0 +1,11 @@
+package Sort;
+
+import java.util.Arrays;
+
+public class SystemSort {
+    public static void main(String[] args) {
+        int[] arr={10,21,45,134,5,11,1};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
