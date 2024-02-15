3905f96ead4874877a77b1457af84080af4cabbd
Suriya <suriyapradeep2003@gmail.com>
2024-02-15 14:32:11 +0530

    Dijkstra Quick Sort

--- src/Sort/DijkstrasQuickSort.java
+++ src/Sort/DijkstrasQuickSort.java
@@ -3,29 +3,26 @@ package Sort;
 import java.util.Arrays;
 
 public class DijkstrasQuickSort<T extends Comparable<T>> {
-    private void swap(T[] arr,int i,int j){
-        T temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
+    private void swap(T[] arr, int i, int j) {
+        T temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
-    public void sort(T[] arr,int low,int high){
+    public void sort(T[] arr, int low, int high) {
         if (high <= low) {
             return;
         }
         int lt = low, gt = high;//lt pivot pointer
         T v = arr[low];
         int i = low;
-        while (i <= gt)
-        {
+        while (i <= gt) {
             int cmp = arr[i].compareTo(v);//Compare pivot and ith variable
             if (cmp < 0) {
                 swap(arr, lt++, i++);//If less swap i with pivot then increment position
-            }
-            else if (cmp > 0) {
+            } else if (cmp > 0) {
                 swap(arr, i, gt--);//If greater swap i with gt decrement gt
-            }
-            else {
+            } else {
                 i++;//Equal means increment i
             }
         }
@@ -34,13 +31,13 @@ public class DijkstrasQuickSort<T extends Comparable<T>> {
     }
 
     public static void main(String[] args) {
-        DijkstrasQuickSort<Integer> dqs1=new DijkstrasQuickSort<>();
-        Integer[] arr={12, 11, 13, 5, 6, 7};
-        dqs1.sort(arr,0,arr.length-1);
+        DijkstrasQuickSort<Integer> dqs1 = new DijkstrasQuickSort<>();
+        Integer[] arr =  {12, 11, 12, 13, 5, 6, 7};
+        dqs1.sort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
         String[] stringArr = {"banana", "apple", "orange", "grape", "kiwi"};
-        DijkstrasQuickSort<String> dqs2=new DijkstrasQuickSort<>();
-        dqs2.sort(stringArr,0,stringArr.length-1); // Corrected here
+        DijkstrasQuickSort<String> dqs2 = new DijkstrasQuickSort<>();
+        dqs2.sort(stringArr, 0, stringArr.length - 1); // Corrected here
         System.out.println(Arrays.toString(stringArr));
     }
 }
